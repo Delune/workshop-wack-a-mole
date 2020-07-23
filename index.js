8754f96953a3392d0d46c588422fea8333012423
Jesse Sullivan <11592718+Delune@users.noreply.github.com>
2020-07-23 10:54:14 -0400

    finished whack-a-mole

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let score = 0;
 var scoreDisplay = document.getElementById('score');
 
-console.log(scoreDisplay);
-//added score log
-console.log('line7', score);
+var holes = document.getElementsByClassName('hole');
+
+setInterval(function () {
+  var randomHoles = Math.floor(Math.random() * holes.length);
+  holes[randomHoles].classList.toggle('mole');
+}, 1000);
+
+var gameArea = document.getElementById('whack-a-mole');
+
+gameArea.addEventListener('click', function (clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    //remove the mole
+    clickEvent.target.classList.remove('mole');
+    //increase the score
+    score++;
+    //display the score
+    scoreDisplay.innerText = score;
+  }
+});
